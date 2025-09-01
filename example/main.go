3f24631ff29cdc250b78f9fd1d6b2fb2f8735436
afif <afif.azizy@gmail.com>
2025-09-01 14:50:49 +0700

    add concurrent communication support

--- example/main.go
+++ example/main.go
@@ -1,21 +1,16 @@
 package main
 
 import (
-	"context"
 	"fmt"
 	"log"
-	"sync"
-	"time"
 
 	simplestdioplugin "github.com/concernedmate/simple-stdio-plugin"
 )
 
 func main() {
-	ctx := context.Background()
-	mapped := simplestdioplugin.PluginMap{Map: &sync.Map{}}
-
+	mapped := simplestdioplugin.NewPluginMap()
 	go func() {
-		if err := simplestdioplugin.PluginRunner(ctx, &mapped, "./plugins", "exe"); err != nil {
+		if err := simplestdioplugin.PluginRunner(&mapped, "./plugins", "exe"); err != nil {
 			log.Fatal(err)
 		}
 	}()
@@ -27,13 +22,62 @@ func main() {
 		}
 
 		if len(plugins) > 0 {
-			result, err := plugins[0].Command([]byte("tes-command"))
-			if err != nil {
-				log.Fatal(err)
-			}
-			fmt.Println("result: ", string(result))
-		}
+			name := plugins[0].Name
 
-		time.Sleep(time.Second)
+			go func() {
+				for {
+					plugin, err := mapped.GetPluginByName(name)
+					if err != nil {
+						fmt.Println(err)
+						continue
+					}
+
+					fmt.Println(name, ": tes-command-1")
+					result, err := plugin.Command([]byte("tes-command-1"))
+					if err != nil {
+						fmt.Println(err)
+						continue
+					}
+					fmt.Println(name, ":", string(result))
+				}
+			}()
+			go func() {
+				for {
+					plugin, err := mapped.GetPluginByName(name)
+					if err != nil {
+						fmt.Println(err)
+						continue
+					}
+
+					fmt.Println(name, ": tes-command-2")
+					result, err := plugin.Command([]byte("tes-command-2"))
+					if err != nil {
+						fmt.Println(err)
+						continue
+					}
+					fmt.Println(name, ":", string(result))
+				}
+			}()
+			go func() {
+				for {
+					plugin, err := mapped.GetPluginByName(name)
+					if err != nil {
+						fmt.Println(err)
+						continue
+					}
+
+					fmt.Println(name, ": tes-command-3")
+					result, err := plugin.Command([]byte("tes-command-3"))
+					if err != nil {
+						fmt.Println(err)
+						continue
+					}
+					fmt.Println(name, ":", string(result))
+				}
+			}()
+
+			break
+		}
 	}
+	select {}
 }
--- example/plugins/main.go
+++ example/plugins/main.go
@@ -3,17 +3,27 @@ package main
 import (
 	"log"
 	"os"
+	"time"
 
 	simplestdioplugin "github.com/concernedmate/simple-stdio-plugin"
 )
 
 func main() {
 	router := map[string]func(jsons []byte) ([]byte, error){
-		"tes-command": func(jsons []byte) ([]byte, error) {
-			return []byte("testing"), nil
+		"tes-command-1": func(jsons []byte) ([]byte, error) {
+			time.Sleep(time.Second)
+			return []byte("testing-1"), nil
+		},
+		"tes-command-2": func(jsons []byte) ([]byte, error) {
+			time.Sleep(time.Second * 2)
+			return []byte("testing-2"), nil
+		},
+		"tes-command-3": func(jsons []byte) ([]byte, error) {
+			time.Sleep(time.Second * 3)
+			return []byte("testing-3"), nil
 		},
 	}
-	plugin := simplestdioplugin.NewPlugin(router, os.Stdin, os.Stdout)
+	plugin := simplestdioplugin.NewPluginClient(router, os.Stdin, os.Stdout)
 	if err := simplestdioplugin.PluginServe(plugin); err != nil {
 		log.Fatal(err)
 	}
