f574aaeef7c436b088b9864e7c2b90a664751081
afif <afif.azizy@gmail.com>
2026-04-30 09:40:00 +0700

    feat: trying to hide terminal on windows

---
+++ exec_windows.go
@@ -0,0 +1,82 @@
+//go:build windows
+
+package simplestdioplugin
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"os/exec"
+	"path"
+	"sync"
+	"syscall"
+)
+
+func execPlugin(ctx context.Context, logger func(string), syncMap *sync.Map, location string, args ...string) error {
+	name := path.Base(location)
+
+	cmd := exec.CommandContext(ctx, location, args...)
+	cmd.SysProcAttr = &syscall.SysProcAttr{CreationFlags: 0x08000000} // CREATE_NO_WINDOW
+	pr1, pw1, err := os.Pipe()
+	if err != nil {
+		return err
+	}
+	pr2, pw2, err := os.Pipe()
+	if err != nil {
+		return err
+	}
+	pr3, pw3, err := os.Pipe()
+	if err != nil {
+		return err
+	}
+
+	cmd.Stdin = pr1
+	cmd.Stdout = pw2
+	cmd.Stderr = pw3
+
+	if err := cmd.Start(); err != nil {
+		return err
+	}
+
+	plugin_running := &PluginRunning{
+		Name: name, Path: location, cmd: cmd, LogFunc: logger,
+		cmd_mutex: sync.RWMutex{}, cmd_map: make(map[string]chan CommandComm),
+		write_chan: make(chan PluginComm), resp_chan: make(chan PluginComm),
+		pipe_in: pw1, pipe_out: pr2, pipe_err: pr3,
+	}
+	logger(fmt.Sprintf("started plugin %s (%s) pid: %d", name, location, cmd.Process.Pid))
+
+	go func() {
+		if err := plugin_running.runner(ctx); err != nil {
+			logger(fmt.Sprintf("plugin runner %s exited: %s", name, err.Error()))
+		}
+	}()
+	go func() {
+		if err := plugin_running.reader(ctx); err != nil {
+			logger(fmt.Sprintf("plugin reader %s exited: %s", name, err.Error()))
+		}
+	}()
+	go func() {
+		if err := plugin_running.stderr(ctx); err != nil {
+			logger(err.Error())
+		}
+	}()
+	go func() {
+		if err := cmd.Wait(); err != nil {
+			logger(fmt.Sprintf("plugin %s exited: %s", name, err.Error()))
+		}
+
+		// cleanup
+		pr1.Close()
+		pw1.Close()
+
+		pr2.Close()
+		pw2.Close()
+
+		close(plugin_running.resp_chan)
+		close(plugin_running.write_chan)
+	}()
+
+	syncMap.Store(name, plugin_running)
+	return nil
+}
--- go.mod
+++ go.mod
@@ -1,5 +1,5 @@
 module github.com/concernedmate/simple-stdio-plugin
 
-go 1.25.5
+go 1.26.1
 
 require github.com/google/uuid v1.6.0
